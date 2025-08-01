d6c88abd10cf338dc56aa303c2112c79d379a9be
Max Dillon <maxdillon13124@gmail.com>
2025-08-01 13:18:45 -0700

    fix: add base path in prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from 'vite';
+import solid from 'vite-plugin-solid';
 
-export default defineConfig({
-  plugins: [solid()],
-  server: {
-    allowedHosts: ['laptop.kinkajou-richter.ts.net'],
-  },
-})
+export default defineConfig(({ command }) => {
+  const isProd = command === 'build';
+
+  return {
+    base: isProd ? '/fast-free-file-transfer/' : '/',
+    plugins: [solid()],
+    server: {
+      allowedHosts: ['laptop.kinkajou-richter.ts.net'],
+    },
+  };
+});
